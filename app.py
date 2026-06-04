a13c292bc2d1635869ad74bac2910cfbe9dc7394
Nadifo Ismail <nadifoismail80@gmail.com>
2026-06-04 22:17:53 +0300

    Made some changes in app.py

--- app.py
+++ app.py
@@ -43,6 +43,9 @@ def home():
 def serve_upload(filename):
     return send_from_directory('uploads', filename)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
